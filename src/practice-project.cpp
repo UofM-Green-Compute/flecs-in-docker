191f476e21eca9650ecbdcd7e224fcbd14c533ec
Mattias Evans <mattiasevans@gmail.com>
2025-10-11 11:27:55 +0100

    Mattias added a comment about make dockerbuild to practice-project.cpp
    Having both main.cpp and practice-project.cpp causes make dockerbuild to see two main() functions and get confused

--- src/practice-project.cpp
+++ src/practice-project.cpp
@@ -1,6 +1,13 @@
+/*
+There seems to be a problem where if you uncomment this code and then try make dockerbuild,
+there is confusion because there are no two main() functions (one here and one in main.cpp).
+
+Not sure what to do about that.
+*/
+
 /* 
 DO NOT BUILD USING VSCODE !!!!!!!!!!
-*/ 
+
 
 #include <iostream>
 #include <flecs.h>
@@ -10,4 +17,5 @@ int main()
 {
     std::cout<<"Hello World"<<std::endl; 
 }
+*/
 
