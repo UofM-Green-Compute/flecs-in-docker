249ccb12f288e0590d7d5eab7e34d9798781785c
Oluwole Delano <oluwoledelano@gmail.com>
2025-10-21 11:18:53 +0100

    Updates: 1)Graph plotting in python functional. 2) Attempt at animation. 3)Use time period to govern no of time steps, the number of steps used is written to the first line of the output file. Note that this can cause annoying indexing issues when plotting if not treated carefully. --Oluwole

--- Sketches/OD/starter/bin/graph-plotting.py
+++ Sketches/OD/starter/bin/graph-plotting.py
@@ -1,22 +1,54 @@
-#import matplotlib.pyplot as plt
-#import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
 
 file = open("SHM-Data.txt")
 
-#x = np.linspace(0,100)
-
+no_points = 0
 pos = []
 vel = []
 acc = []
 
+for i,row in enumerate(file): 
+    if (i == 0):
+        no_points = int(row)
+    else:
+        row = row.split(",")
+        pos.append(float(row[0]))
+        vel.append(float(row[1]))
+        acc.append(float(row[2])) 
 
+t = np.linspace(0,no_points,no_points)
 
-for row in file: 
-    row = row.split(",")
-    pos.append(row[0])
-    vel.append(row[1])
-    acc.append(row[2]) 
+fig, ax = plt.subplots(3)
+plt.suptitle("Simple Harmonic Oscillator")
 
-print(pos)
+ax[0].plot(t,pos,label="Position")
+ax[0].set_title("Position")
+ax[1].plot(t,vel,color='Orange',label="Velocity")
+ax[1].set_title("Velocity")
+ax[2].plot(t,acc,color='Red',label="Acceleration")
+ax[2].set_title("Acceleration")
+fig.tight_layout()
 
-#plt.plot(x,pos)
+for ax in fig.get_axes():
+    ax.label_outer()
+
+artists = []
+
+"""
+ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=400)
+
+fig, ax = plt.subplots()
+
+def animate(i):
+    ax.clear()
+    point = pos[i]
+    ax.plot(point,0,color = 'red', marker = 'o')
+    ax.set_xlim([-5, 100])
+    ax.set_ylim([-5, 5])
+
+ani = animation.FuncAnimation(fig, animate, frames=no_points,interval=5, repeat=False)
+#plt.close()
+"""
+plt.show()
