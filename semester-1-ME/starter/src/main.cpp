d3585be392bdd25cae9f97ee92e15233c679a45d
Mattias Evans <mattiasevans@gmail.com>
2025-10-23 14:25:46 +0100

    Finished Coupled Spring Code for 2 springs

--- semester-1-ME/starter/src/main.cpp
+++ semester-1-ME/starter/src/main.cpp
@@ -1,46 +1,51 @@
 /*
+#include <iostream>
 #include <flecs.h>
 #include <systems.h>
-#include <iostream>
+
+double k = 2; // Spring Constant in Nm-1
 
 struct Position { 
-    double x, y;
+    double x; // In cm
 };
 
 struct Velocity { 
-    double x, y;
+    double x; // In cms-1
 };
 
-int main(int, char *[]) {
-    flecs::world ecs;
+struct Acceleration{
+    double x; // in cms-2
+};
 
-    // Create a system for Position, Velocity. Systems are like queries (see
-    // queries) with a function that can be ran or scheduled (see pipeline).
-    flecs::system s = ecs.system<Position, const Velocity>()
-        .each([](flecs::entity e, Position& p, const Velocity& v) {
+struct Mass{
+    double m; // in kg
+};
+
+
+int main() {
+    flecs::world ecs;
+    // Create a system for Position, Velocity, Acceleration..
+    flecs::system s = ecs.system<Position, Velocity, Acceleration, const Mass>()
+        .each([](flecs::entity e, Position& p, Velocity& v, Acceleration& a, const Mass& mass) {
+            a.x = - (k*p.x)/mass.m;
+            v.x += a.x;
             p.x += v.x;
-            p.y += v.y;
-            std::cout << e.name() << ": {" << p.x << ", " << p.y << "}\n";
+            std::cout << e.name() << ": {" << p.x << ", " << v.x << "," << a.x << "}\n";
         });
 
-    // Create a few test entities for a Position, Velocity query
     ecs.entity("e1")
-        .set<Position>({10, 20})
-        .set<Velocity>({1, 2});
+        .set<Position>({0})
+        .set<Velocity>({1})
+        .set<Acceleration>({0})
+        .set<Mass>({3});
 
-    ecs.entity("e2")
-        .set<Position>({10, 20})
-        .set<Velocity>({3, 4});
-
-    // This entity will not match as it does not have Position, Velocity
-    ecs.entity("e3")
-        .set<Position>({10, 20});
     
+    std::cout<<"Hello World"<<std::endl; 
 
-    // // Run the system 10 times
     for(auto iter=10; iter--;) {
-         s.run();
-         std::cout << "----\n";
-     }
+        s.run();
+        std::cout << "----\n";
+    }
+    
 }
 */
