8c8b0e56f33c7904a9f40ffa694b747da73784ff
Mattias Evans <mattiasevans@gmail.com>
2025-10-28 11:24:45 +0000

    Added python environment to my sketches. changed the file opening code to be more general in python

---
+++ semester-1-ME/starter/src/graph-plotting.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import PillowWriter
+from matplotlib.animation import FuncAnimation 
+import os
+
+# Open data file
+
+root_folder = os.path.dirname(os.path.dirname(__file__))
+data_path = os.path.join(root_folder, "outputs", "Coupled_Oscillators.txt")
+file = open(data_path)
+
+# Define number of data points variable, arrays to store components
+no_points = 0
+time = []
+
+pos0 = []
+vel0 = []
+acc0 = []
+
+pos2 = []
+vel2 = []
+acc2 = []
+
+positions = [pos0,pos2]
+velocities = [vel0,vel2]
+accelerations = [acc0,acc2]
+
+# Read data in from file
+for i,row in enumerate(file):
+
+    if (i == 0):
+        row = row.split(": ")
+        no_points = int(row[1])
+        print(no_points)
+    elif(i == 1):
+        print(row)
+    else:
+        row = row.split(",")
+        time.append(float(row[0]))
+        pos0.append(float(row[1]))
+        vel0.append(float(row[2]))
+        acc0.append(float(row[3])) 
+
+        pos2.append(float(row[4]))
+        vel2.append(float(row[5]))
+        acc2.append(float(row[6])) 
+
+zeroes = np.zeros((no_points,1))
+
+# Graph formatting
+fig, [ax_pos, ax_vel, ax_acc, ax_ani] = plt.subplots(4)
+plt.suptitle("Coupled Oscillator")
+
+ax_pos.set_title("Position")
+ax_pos.set_xlim(0,float(max(time)))
+ax_pos.set_ylim(min(min(pos0),min(pos2))-1,max(max(pos0),max(pos2))+1)
+
+ax_vel.set_title("Velocity")
+ax_vel.set_xlim(0,float(max(time))+1)
+ax_vel.set_ylim(min(min(vel0),min(vel2))-1,max(max(vel0),max(vel2))+1)
+
+ax_acc.set_title("Acceleration")
+ax_acc.set_xlim(0,float(max(time))+1)
+ax_acc.set_ylim(min(min(acc0),min(acc2))-1,max(max(acc0),max(acc2))+1)
+
+ax_ani.set_title("Particle")
+ax_ani.set_xlim(min(min(pos0),min(pos2))-1,max(max(pos0),max(pos2))+1)
+ax_ani.set_ylim(-1,1)
+
+fig.tight_layout()
+
+# Animation
+graph0, = ax_pos.plot([],[],label="Position")
+graph1, = ax_vel.plot([],[],color="Orange",label="Velocity")
+graph2, = ax_acc.plot([],[],color="Red",label="Velocity")
+graph3, = ax_ani.plot(0,0,'o',color="Red")
+graph4, = ax_ani.plot(0,0,'o',color="Blue")
+
+for ax in fig.get_axes():
+   ax.label_outer()  
+
+def all_positions(frame):
+    for p in positions:
+        line = ax_pos.plot(time[:frame+1],p[:frame+1],color='blue',label="Position")  
+
+def all_velocities(frame):
+    for v in velocities:
+        line = ax_vel.plot(time[:frame+1],v[:frame+1],color='orange',label="Velocity") 
+
+def all_accelerations(frame):
+    for a in accelerations:
+        line = ax_acc.plot(time[:frame+1],a[:frame+1],color='red',label="Acceleration")  
+
+def particle(frame):
+    graph3.set_data([pos0[frame]],zeroes[frame])
+    graph4.set_data([pos2[frame]],zeroes[frame])
+
+ani = FuncAnimation(fig, all_positions, frames=len(time), interval=10)
+ani1 = FuncAnimation(fig, all_velocities, frames=len(time), interval=10)
+ani2 = FuncAnimation(fig, all_accelerations, frames=len(time), interval=10)
+ani3 = FuncAnimation(fig, particle, frames=len(time), interval=10)
+
+plt.show()
+
+# Making a gif
+metadata = dict(title="Movie", artist="Oluwole")
+writer = PillowWriter(fps=100, metadata=metadata)
+
+time_list = []
+pos_list = [] 
+vel_list = []
+acc_list = []
+zero = []
+
+with writer.saving(fig, "Oscillator.gif", 100):
+    for i,time in enumerate(time):
+        time_list.append(time)
+        pos_list.append(pos0[i])
+        vel_list.append(vel0[i])
+        acc_list.append(acc0[i])
+        zero.append(0)
+
+        graph0.set_data(time_list,pos_list)
+        graph1.set_data(time_list,vel_list)
+        graph2.set_data(time_list,acc_list)
+        graph3.set_data(pos_list[-1:],zero[-1:])
+        writer.grab_frame()
--- semester-1-ME/starter/src/springs.cpp
+++ semester-1-ME/starter/src/springs.cpp
@@ -16,12 +16,18 @@ Entity types:
 #include <iostream>
 #include <fstream> 
 #include <vector>
+#include <cmath>
 #include <flecs.h>
 #include <systems.h>
 
-int N = 2; //Particle Number
-float time_step = 0.001; //How much time passes in each iteration
-int run_time = 1000; 
+double k_freq_calc = 3; 
+double particle_mass = 5; 
+float w1 = std::sqrt( k_freq_calc / particle_mass); // Frequency of the mass-spring system 
+int time_period = ( (2 * M_PI) / w1 ); // Time the spring system runs for in s 
+float time_step = 0.01; // Time elapsed in each time step 
+int run_time = time_period * 1/time_step; // Number of loops to run 
+
+int N = 2; // Number of particles in the system
 
 //Position of the Left Wall
 double p_Lwall = 0;  // in cm
@@ -32,26 +38,24 @@ double p_Rwall = 12; // in cm
 struct Index {
     int i; // Which particle is it
 };
-
 struct Position { 
     double x; // In cm
 };
-
 struct Velocity { 
-    double x;// In cms-1
+    double x; // In cms-1
 };
-
 struct Acceleration{
     double x; // in cms-2
 };
-
 struct Mass{
     double M; // in kg
 };
 
-double find_acceleration(float position, float position_left, float position_right,
-const float mass, float k_left, float k_right, float l_left, float l_right){
+struct Particle {}; 
 
+double acceleration(float position, float position_left, float position_right,
+const float mass, float k_left, float k_right, float l_left, float l_right)
+{
     float acc;
     acc = - (k_left/mass) * (position - position_left - l_left) 
         + (k_right/mass) * (position_right - position - l_right);
@@ -59,58 +63,60 @@ const float mass, float k_left, float k_right, float l_left, float l_right){
     return acc;
 }
 
-
 int main() {
-    std::ofstream MyFile("Coupled_Oscillators.txt");
+
+    // Open file for writing
+    std::ofstream MyFile; 
+    MyFile.open("Coupled_Oscillators.txt");
+    if (!MyFile.is_open())
+    {
+        std::cout<<"Error in creating file"<<std::endl; 
+        return 1; 
+    }
+    MyFile << "No iterations: " << run_time << std::endl;
+    MyFile << "Time (s), Position 1 (cm), Velocity 1 (cm s-1), Acceleration 1 (cm s-2)" 
+           << ", Position 2 (cm), Velocity 2 (cm s-1), Acceleration 2 (cm s-2)" << std::endl;
 
     // First Particle Data
     std::vector<std::vector<double>> p_matrix {{2}, {7}}; 
     std::vector<std::vector<double>> v_matrix {{2}, {-1}};
     std::vector<std::vector<double>> a_matrix {{0}, {0}};
 
-    //Spring Constant Data
-    const std::vector<double> k_list {3, 3, 3};
-    // Natural Spring Length Data
-    const std::vector<double> l_list {4, 4, 4};
-
-    flecs::world ecs;
+    const std::vector<double> k_list {3, 3, 3}; // Spring Constants
+    const std::vector<double> l_list {4, 4, 4}; // Natural Spring Lengths
     
-    /*
-        System s1 runs for any entity with the tag Particle
-        Its job is to calculate the initial acceleration and
-        update the vectors and entity components. 
-    */
+    flecs::world ecs;
+
     flecs::system s1 = ecs.system<Acceleration, Position, Mass, Index>()
-    .each([&](Acceleration &a, Position &p, const Mass &mass, const Index &index) {
+    .each([&](Acceleration &a, Position &p, const Mass &mass, const Index &index) 
+    {
         if (index.i == 0) {
-            a.x = find_acceleration(p.x, p_Lwall, p_matrix[index.i+1][0], mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_Lwall, p_matrix[index.i+1][0], mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } else if (index.i == N-1) {
-            a.x = find_acceleration(p.x, p_matrix[index.i-1][0], p_Rwall, mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_matrix[index.i-1][0], p_Rwall, mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } else {
-            a.x = find_acceleration(p.x, p_matrix[index.i-1][0], p_matrix[index.i+1][0], mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_matrix[index.i-1][0], p_matrix[index.i+1][0], mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } 
         a_matrix[index.i][0] = a.x;
-        
     });
-    
-    
+
     flecs::system s2 = ecs.system<Position, Velocity, Acceleration, Mass, Index>()
-    .each([&](flecs::entity e, Position &p, Velocity &v, Acceleration &a, const Mass &mass, const Index &index) {
-        
+    .each([&](flecs::entity e, Position &p, Velocity &v, Acceleration &a, const Mass &mass, const Index &index) 
+    {
         p.x += v.x*time_step;
         v.x += a.x*time_step;
         
         if (index.i == 0) {
-            a.x = find_acceleration(p.x, p_Lwall, p_matrix[index.i+1][0], mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_Lwall, p_matrix[index.i+1][0], mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } else if (index.i == N-1) {
-            a.x = find_acceleration(p.x, p_matrix[index.i-1][0], p_Rwall, mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_matrix[index.i-1][0], p_Rwall, mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } else {
-            a.x = find_acceleration(p.x, p_matrix[index.i-1][0], p_matrix[index.i+1][0], mass.M, k_list[index.i], 
+            a.x = acceleration(p.x, p_matrix[index.i-1][0], p_matrix[index.i+1][0], mass.M, k_list[index.i], 
                                     k_list[index.i+1], l_list[index.i], l_list[index.i+1]);
         } 
         
@@ -119,40 +125,52 @@ int main() {
         a_matrix[index.i].push_back(a.x);
         
         std::cout << e.name() << ": {" << p.x << ", " << v.x << "," << a.x << "}\n";
-        
     });
-    
-    // Create Entities
-    ecs.entity("mass 1")
-        // Finds and sets components
+
+    // Particle prefab, a template that we can use to generate entities
+    ecs.prefab<Particle>()
+        .set<Index>({})
+        .set<Mass>({})
+        .set<Position>({})
+        .set<Velocity>({})
+        .set<Acceleration>({});
+
+    flecs::entity particle0 = ecs.entity().is_a<Particle>()
         .set<Index>({0})
         .set<Mass>({4})
         .set<Position>({p_matrix[0][0]})
-        .set<Velocity>({v_matrix[0][0]})
-        .set<Acceleration>({0});  
-    
-    ecs.entity("mass 2")
-        // Finds and sets components
+        .set<Velocity>({v_matrix[0][0]});
+
+    flecs::entity particle1 = ecs.entity().is_a<Particle>()
         .set<Index>({1})
         .set<Mass>({4})
         .set<Position>({p_matrix[1][0]})
-        .set<Velocity>({v_matrix[1][0]})
-        .set<Acceleration>({0});
-    
+        .set<Velocity>({v_matrix[1][0]});
+
+    // *** This loop wasn't working as expected. Led to incorrect data beign written in file
+    // for(int i = 0; i < N; i++)
+    // {
+    //     flecs::entity particle = ecs.entity().is_a(Particle); 
+    //     particle.set(Index{i});
+    //     particle.set(Position{p_matrix[0][0]});
+    //     particle.set(Velocity{v_matrix[0][0]});
+    //     particle.set(Acceleration{0}); 
+    //     particle.set(Mass{particle_mass});
+    // }
+
     s1.run();
 
-    MyFile << "Time (s), Position 1 (cm), Velocity 1 (cm s-1), Acceleration 1 (cm s-2)" 
-           << ", Position 2 (cm), Velocity 2 (cm s-1), Acceleration 2 (cm s-2)" << std::endl; 
-    MyFile << 0 << ", " << p_matrix[0][0] << ", " << v_matrix[0][0] << "," << a_matrix[0][0] << "," 
-           << p_matrix[1][0] << ", " << v_matrix[1][0] << "," << a_matrix[1][0] << std::endl; 
-
-    for(int iter = 1; iter < run_time; iter++) {
+    // Run the system
+    for(auto iter=run_time; iter--;) 
+    {
         s2.run();
         std::cout << "----\n";
-        
-        MyFile << iter*time_step << ", " << p_matrix[0][iter] << ", " << v_matrix[0][iter] << "," << a_matrix[0][iter] << "," 
-               << p_matrix[1][iter] << ", " << v_matrix[1][iter] << "," << a_matrix[1][iter] << std::endl; 
-        
+    }
+    for(int i = 0; i < run_time; i++) 
+    {
+        MyFile << i*time_step << ", " << p_matrix[0][i] << ", " << v_matrix[0][i] << "," << a_matrix[0][i] << "," 
+               << p_matrix[1][i] << ", " << v_matrix[1][i] << "," << a_matrix[1][i] << std::endl; 
     }
     MyFile.close();
+    
 }
