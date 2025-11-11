cd7093aa8ad2a9b2f31679a84a18aa64143d213e
Oluwole Delano <oluwoledelano@gmail.com>
2025-11-11 22:35:02 +0000

    11/11/25 22:33 OD Trying to finalise Gauss elimination. Code has bug(s) that are giving an incorrect solution. I think this likely comes down to errors in my indexing in summations, matrix multiplications etc.

--- Sketches/OD/first_fluid/src/gauss-elimination.cpp
+++ Sketches/OD/first_fluid/src/gauss-elimination.cpp
@@ -2,12 +2,16 @@
 Program to trial gauss elimination
 
 1. Gauss elimination doesn't know what to do when one of the elements of the matrix is 0
+    There may be an issue with my algorithm... there shouldn't be any zeroes on the diagonal and hence should be
+    no /0 issues
     If the element we hit is 0, swap the order of two rows
     This article looks helpful: https://www.statlect.com/matrix-algebra/Gaussian-elimination
 2. Passing "Matrix[j]" into row_manipulation twice? 
 3. Double check that the gauss elimination, diagonals_to_matrix functions are rigorous 
 
 */
+
+/* 
 
 #include <algorithm>
 #include <vector>
@@ -29,9 +33,8 @@ double Gamma = (Rho * u * Nx) / Pe;
 double Start = 0;
 double End = 1;
 
-// Matrix struct (OD used in my first version of the code)
+// Matrix struct (OD: Used in my first version of the code)
 struct Matrix { std::vector<std::vector<double>> M; }; 
-
 // Create structs to be used as components for the matrix 
 struct West { std::vector<double> a; };
 struct Diagonal { std::vector<double> b; };
@@ -40,7 +43,7 @@ struct Qvector { std::vector<double> q; };
 struct Conserved { std::vector<double> phi; };
 struct MatrixTag {};
 
-// - Mattias 
+// 
 double west_function(int left, int centre, int right)
 {
     float A;
@@ -48,7 +51,7 @@ double west_function(int left, int centre, int right)
     return A;
 }
 
-// - Mattias 
+// 
 double east_function(int left, int centre, int right)
 {
     float A;
@@ -75,14 +78,14 @@ std::vector<std::vector<double>> diagonals_to_matrix(std::vector<double> main_di
     return matrix; 
 }
 
-// Function to perform gauss elimination operations on each row of the matrix 
-std::vector<double> row_manipulation(std::vector<double> ith_row, std::vector<double> first_row, int column)
+// Function to print elements of a vector - Oluwole
+void print_vector(std::vector<double> vector)
 {
-    for (int i = column; i < ith_row.size(); i++)
-    {
-        ith_row[i] = ith_row[i] - first_row[i] * ith_row[column] / first_row[column]; 
-    }
-    return ith_row; 
+    std::cout<<"---------------------------------"<<std::endl; 
+    std::cout<<"{ ";
+    for(int j = 0; j < vector.size(); j++) { std::cout<<vector[j]<<" "; }
+    std::cout<<"}"<<std::endl;
+    std::cout<<"---------------------------------"<<std::endl;
 }
 
 // Function to print elements of a matrix 
@@ -97,14 +100,58 @@ void print_matrix(std::vector<std::vector<double>> matrix)
     std::cout<<"-------"<<std::endl;
 }
 
-// Function to print elements of a vector - Oluwole
-void print_vector(std::vector<double> vector)
+std::vector<std::vector<double>> augmented_matrix(std::vector<std::vector<double>> Matrix, std::vector<double> Vector)
 {
-    std::cout<<"---------------------------------"<<std::endl; 
-    std::cout<<"{";
-    for(int j = 0; j < vector.size(); j++) { std::cout<<vector[j]; }
-    std::cout<<"}"<<std::endl;
-    std::cout<<"---------------------------------"<<std::endl;
+    if(Matrix.size() == Vector.size())
+    {
+        for (int i = 0; i < Matrix.size(); i++)
+        { Matrix[i].push_back(Vector[i]); }
+    }
+    else
+    {
+        std::cout<<"*** ERROR: Incompatible matrix and vector for augmented matrix ***"<<std::endl; 
+    }
+    return Matrix; 
+}
+
+// Function to perform gauss elimination operations on each row of the matrix 
+std::vector<double> row_manipulation(std::vector<double> ith_row, std::vector<double> row_to_manipulate, int column)
+{
+    // ith_row: We manipulate the matrix such that entries below ith_row[column] = 0
+    // row_to_manipulate: This is the row we are manipulating. At the end of the procedure row_to_manipulate[column] = 0.
+    
+    for (int i = 0; i < row_to_manipulate.size(); i++)
+    {
+        row_to_manipulate[i] = row_to_manipulate[i] - ith_row[i] * row_to_manipulate[column] / ith_row[column]; 
+    }
+    int size = row_to_manipulate.size(); 
+    row_to_manipulate[size-1] = row_to_manipulate[size-1] - ith_row[size-1] * row_to_manipulate[column] / ith_row[column];
+
+    return row_to_manipulate; 
+}
+
+std::vector<double> solve_equation(std::vector<std::vector<double>> Matrix)
+{
+    std::vector<double> phi = {}; 
+    int no_rows = Matrix.size(); 
+    int no_columns = Matrix[0].size();
+     
+    // nth case 
+    phi.push_back(Matrix[no_rows-1][no_columns-1]); 
+
+    for(int i = no_rows-2;  i > 0; i--)
+    {
+        double sum = 0;
+        for(int k = i+1; k < no_columns-1; k++)
+        { 
+            std::cout<<k<<","<<phi[(no_rows-1)-k]<<std::endl;
+            sum +=  Matrix[i][k] * phi[(no_rows-1)-k]; 
+        } 
+        std::cout<<sum<<std::endl;
+        // std::cout<<Matrix[i][no_columns-1]<<std::endl;
+        phi.push_back( (Matrix[i][no_columns-1] - sum) / Matrix[i][i]); 
+    }
+    return phi; 
 }
 
 int main() {
@@ -119,12 +166,6 @@ int main() {
     world.component<Qvector>();
     world.component<Conserved>();
     world.component<MatrixTag>();
-
-    // Create matrix 
-    std::vector<std::vector<double>> matrix = {{1,-3,4},{2,-5,6},{-3,3,4}}; 
-    //print_matrix(matrix); 
-    world.entity("Matrix 1")
-        .set<Matrix>({matrix});
     
     // Create the Matrix as an entity
     world.entity("Matrix")
@@ -161,21 +202,21 @@ int main() {
                     east.c.push_back(A_E);
                     Qvec.q.push_back(0);    
                 }
-
             }
             conserved.phi.push_back(End);
+            std::cout<<"Initial Conserved Quantity: "<<std::endl; 
             print_vector(conserved.phi); 
+
         });
 
-    // System to Compute value of conserved quantity inside system
+    // System to Compute value of conserved quantity 
     world.system<West, Diagonal, East, Qvector, Conserved>()
         .with<MatrixTag>()
         .kind(flecs::OnUpdate)
         .each([&](West& west, Diagonal& diag, East& east, Qvector& Qvec, Conserved& conserved){
 
-            std::cout<<"Please please please work I'm Sabrina Carpenter"<<std::endl; 
-
             std::vector<std::vector<double>> Matrix = diagonals_to_matrix(diag.b, east.c, west.a); 
+            Matrix = augmented_matrix(Matrix, Qvec.q); 
 
             int no_rows = Matrix.size();
             int no_columns = Matrix[0].size(); 
@@ -184,22 +225,29 @@ int main() {
             {
                 for(int j = i+1; j < no_rows; j++)
                 {
-                Matrix[j] = row_manipulation(Matrix[j], Matrix[j], i);
+                    Matrix[j] = row_manipulation(Matrix[i], Matrix[j], i);
                 }
             }
 
-            print_matrix(Matrix); 
+            // print_matrix(Matrix); 
+            conserved.phi = solve_equation(Matrix); 
+            std::cout<<"Solved Conserved Quantity: "<<std::endl; 
+            print_vector(conserved.phi);  
+
+            std::cout<<"Final (Augmented) Matrix: "<<std::endl; 
+            print_matrix(Matrix);
 
         });
         
     flecs::system matrix_solver = world.system<Matrix>() 
     .each([&](Matrix &matrix)
     {
-        
         // print_matrix(matrix.M);
     });
 
     world.progress(); 
-    initialise_matrix.run(); 
-    matrix_solver.run(); 
+    // initialise_matrix.run(); 
+    // matrix_solver.run(); 
 }
+
+*/
