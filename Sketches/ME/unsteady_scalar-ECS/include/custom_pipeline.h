670a114da11cd94ae285c4b4e82990db72a8075f
Mattias Evans <mattiasevans@gmail.com>
2025-11-13 13:29:12 +0000

    ME 13/11/2025 13:29: Tried to add a custom pipeline but systems don't seem to be running

---
+++ Sketches/ME/unsteady_scalar-ECS/include/custom_pipeline.h
@@ -0,0 +1,14 @@
+#ifndef CUSTOM_PIPELINE_H
+#define CUSTOM_PIPELINE_H
+
+#include "custom_pipeline/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Sketches/ME/unsteady_scalar-ECS/include/custom_pipeline/bake_config.h
@@ -0,0 +1,6 @@
+#ifndef CUSTOM_PIPELINE_BAKE_CONFIG_H
+#define CUSTOM_PIPELINE_BAKE_CONFIG_H
+
+#include <flecs.h>
+
+#endif
