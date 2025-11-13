9d7eab99bfa63d42e7ce678a769f4f2182c1c175
Mattias Evans <mattiasevans@gmail.com>
2025-11-13 13:29:12 +0000

    ME 13/11/2025 13:29: Tried to add a custom pipeline but systems don't seem to be running

--- semester-1-ME/unsteady_scalar-ECS/src/main.cpp
+++ semester-1-ME/unsteady_scalar-ECS/src/main.cpp
@@ -3,6 +3,7 @@ This code runs a simulation of a simple fluid which can be
 solved analytically.
 */
 
+#include <custom_pipeline.h>
 #include <iostream>
 #include <vector>
 #include <fstream> 
@@ -10,6 +11,9 @@ solved analytically.
 #include <systems.h>
 #include <cmath>
 
+// Sets time parameters
+int STEPS = 100;
+int TIME = 60;
 
 // Number of nodes in x-axis. Also equal to the length in unit of node distance
 const double L = 1; // Length
@@ -25,6 +29,14 @@ double GAMMA = (RHO * U * L) / PE;
 double Start = 0;
 double End = 1;
 
+// Pipeline structs
+struct Save {};
+struct RungeKutta_1 {};
+struct RungeKutta_2 {};
+struct RungeKutta_3 {};
+struct RungeKutta_4 {};
+struct Update {};
+
 // Position Component
 struct position {double x; };
 
@@ -39,25 +51,20 @@ struct phinext_final { double i; };
 struct Middle_Tag {};
 struct End_Tag {};
 
-double linear_function
-(
-    double x, double A, double B
-)
+double linear_function(double x, double A, double B)
 {
     double m = (B-A)/L;
     double y = m * x;
     return y;
 }
 
-double fluid_function
-(
-    double phi_left, double phi, double phi_right, 
-    double x_left, double x, double x_right
-)
+double fluid_function(double phi_left, double phi, double phi_right, 
+    double x_left, double x, double x_right)
 {
     double f;
     f = - U*((phi_right + phi_left)/(x_right-x_left)) 
     + (GAMMA/RHO) * (phi_right - 2 * phi + phi_left) / ((x_right - x) * (x - x_left));
+    return f;
 }
 
 int main(int argc, char* argv[]) {
@@ -73,6 +80,18 @@ int main(int argc, char* argv[]) {
     // Create the World
     flecs::world world(argc, argv);
 
+    // Set up custom pipeline
+    flecs::entity pipeline = world.pipeline()
+        .with(flecs::System)
+        .with<Save>()
+        .with<RungeKutta_1>()
+        .with<RungeKutta_2>()
+        .with<RungeKutta_3>()
+        .with<RungeKutta_4>()
+        .with<Update>()
+        .build();
+    world.set_pipeline(pipeline);
+
     // Create components inside the world
     world.component<Middle_Tag>();
     world.component<End_Tag>();
@@ -84,8 +103,9 @@ int main(int argc, char* argv[]) {
     world.component<phihalf_second>();
     world.component<phinext_first>();
     world.component<phinext_final>();
-    std::vector<flecs::entity> nodes;
-    nodes.reserve(N);
+
+    std::vector<flecs::entity> nodes; // place to store nodes
+    nodes.reserve(N); // Create the space
     
     nodes.push_back(
             world.entity()
