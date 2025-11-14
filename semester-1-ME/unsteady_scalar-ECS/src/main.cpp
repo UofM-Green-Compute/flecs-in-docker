5dc84b96ea44dca77c4146bff8b9f89bc65548f2
Mattias Evans <mattiasevans@gmail.com>
2025-11-14 16:23:35 +0000

    ME 14/11/2025 16:23: First attempt at coding the dynamic case
    Got a warning saying parameter half set but not used - even though i have used it?!

--- semester-1-ME/unsteady_scalar-ECS/src/main.cpp
+++ semester-1-ME/unsteady_scalar-ECS/src/main.cpp
@@ -14,7 +14,7 @@ int TIME = 1;
 
 // Number of nodes in x-axis. Also equal to the length in unit of node distance
 const double L = 1; // Length
-const double N = 41; // Number of nodes
+const int N = 41; // Number of nodes
 const int PE = 50; // Peclet Number
 const double RHO = 2; // Density
 const double U = 5; // Speed
@@ -26,8 +26,9 @@ double GAMMA = (RHO * U * L) / PE;
 double Start = 0;
 double End = 1;
 
-// Position Component
+// Position Components
 struct Position {double x; };
+struct Index {int ind; };
 
 // Field components
 struct Phi_start { double i; };
@@ -59,7 +60,7 @@ double fluid_function(double phi_left, double phi, double phi_right,
 int main(int argc, char *argv[]) {
     // Prepare Save File
     std::ofstream MyFile;
-    MyFile.open("1D_dynamic_fluidtxt");
+    MyFile.open("1D_dynamic_fluid.txt");
 
     // Check Save File is open/created
     if (!MyFile.is_open())
@@ -71,13 +72,8 @@ int main(int argc, char *argv[]) {
     flecs::world world(argc, argv);
 
     // Creates Phases which tell the program in which order to run the systems
-    /*
-    flecs::entity Save = world.entity()
-        .add(flecs::Phase); // This Phase saves the state in its current form
-    */
     flecs::entity RungeKutta_1 = world.entity()
         .add(flecs::Phase); // This Phase calculates phihalf_predict
-        //.depends_on(Save);
 
     flecs::entity RungeKutta_2 = world.entity()
         .add(flecs::Phase) // This Phase calculates phihalf_correct
@@ -107,10 +103,6 @@ int main(int argc, char *argv[]) {
     world.component<Phi_end_predict>();
     world.component<Phi_end_correct>();
 
-    // Creates Neighbour Relationships
-    auto Left_Neighbour = world.entity();
-    auto Right_Neighbour = world.entity();
-
     std::vector<flecs::entity> nodes; // place to store nodes
     nodes.reserve(N); // Create the space
     
@@ -119,6 +111,7 @@ int main(int argc, char *argv[]) {
             world.entity()
                 .add<End_Tag>() 
                 .set<Position>({0})
+                .set<Index>({0})
                 .set<Phi_start>({Start})
                 .set<Phihalf_predict>({Start})
                 .set<Phihalf_correct>({Start})
@@ -133,6 +126,7 @@ int main(int argc, char *argv[]) {
             world.entity()
                 .add<Middle_Tag>() 
                 .set<Position>({index*L/N})
+                .set<Index>({index})
                 .set<Phi_start>({phi_initial})
                 .set<Phihalf_predict>({0})
                 .set<Phihalf_correct>({0})
@@ -146,60 +140,116 @@ int main(int argc, char *argv[]) {
             world.entity()
                 .add<End_Tag>() 
                 .set<Position>({L})
+                .set<Index>({N-1})
                 .set<Phi_start>({End})
                 .set<Phihalf_predict>({End})
                 .set<Phihalf_correct>({End})
                 .set<Phi_end_predict>({End})
             );
     
-    
-
-    // This loop adds left and right neighbours to each non-boundary node
-    
-
-    for (int index = 1; index < N-1; ++index) {
-        nodes[index].add(Left_Neighbour, nodes[index-1]);
-        nodes[index].add(Right_Neighbour, nodes[index+1]);
-    }
-
-    // This system saves phi_start to the txt file
-    /*
-    world.system<Position, Phi_start>()
-        .kind(Save)
-        .each([&](Position pos, Phi_start phi){
-            if (pos.x == L) {
-                MyFile << phi.i << "\n";
-            } else {
-                MyFile << phi.i << ", ";
-            }
-
-        });
-    */
-
     // This system finds and updates phihalf_predict
-    world.system<Position, Phi_start, Phihalf_predict>()
+    world.system<Position, Index, Phi_start, Phihalf_predict>()
         .with<Middle_Tag>()
         .kind(RungeKutta_1)
-        .each([&](Position pos, Phi_start phi0, Phihalf_predict phi_half){
-            double Phi_left;
+        .each([&](Position pos, Index index, Phi_start start, Phihalf_predict half){
+            const Phi_start& startMinus = nodes[index.ind - 1].get<Phi_start>();
+            const Phi_start& startPlus = nodes[index.ind + 1].get<Phi_start>();
+
+            const Position& posMinus = nodes[index.ind - 1].get<Position>();
+            const Position& posPlus = nodes[index.ind + 1].get<Position>();
+
+            double function = fluid_function(startMinus.i, start.i, startPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+
+            half.i = start.i + (TIME*function)/(2*STEPS);
         });
     
+    /*
     // This system finds and updates phihalf_correct
-    world.system()
-        .kind(RungeKutta_2);
+    world.system<Position, Index, Phi_start, Phihalf_predict, Phihalf_correct>()
+        .with<Middle_Tag>()
+        .kind(RungeKutta_2)
+        .each([&](Position pos, Index index, Phi_start start, 
+            Phihalf_predict predictor, Phihalf_correct corrector){
+
+            const Phihalf_predict& predictorMinus = nodes[index.ind - 1].get<Phihalf_predict>();
+            const Phihalf_predict& predictorPlus = nodes[index.ind + 1].get<Phihalf_predict>();
+
+            const Position& posMinus = nodes[index.ind - 1].get<Position>();
+            const Position& posPlus = nodes[index.ind + 1].get<Position>();
+
+            double function = fluid_function(predictorMinus.i, predictor.i, predictorPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+
+            corrector.i = start.i + (TIME*function)/(2*STEPS);
+        });
     
     // This system finds and updates phi_end_predict
-    world.system()
-        .kind(RungeKutta_3);
+    world.system<Position, Index, Phi_start, Phihalf_correct, Phi_end_predict>()
+        .with<Middle_Tag>()
+        .kind(RungeKutta_3)
+        .each([&](Position pos, Index index, Phi_start start, 
+            Phihalf_correct half, Phi_end_predict end){
+
+            const Phihalf_correct& halfMinus = nodes[index.ind - 1].get<Phihalf_correct>();
+            const Phihalf_correct& halfPlus = nodes[index.ind + 1].get<Phihalf_correct>();
+
+            const Position& posMinus = nodes[index.ind - 1].get<Position>();
+            const Position& posPlus = nodes[index.ind + 1].get<Position>();
+
+            double function = fluid_function(halfMinus.i, half.i, halfPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+
+            end.i = start.i + (TIME*function)/(2*STEPS);
+        });
 
     // This system finds and updates phi_end_correct
-    world.system()
-        .kind(RungeKutta_4);
+    world.system<Position, Index, Phi_start, Phihalf_predict, Phihalf_correct, Phi_end_predict, Phi_end_correct>()
+        .with<Middle_Tag>()
+        .kind(RungeKutta_4)
+        .each([&](Position pos, Index index, Phi_start start, Phihalf_correct halfPred,
+            Phihalf_correct halfCorr, Phi_end_predict endPred, Phi_end_correct endCorr){
+            
+            const Phi_start& startMinus = nodes[index.ind - 1].get<Phi_start>();
+            const Phi_start& startPlus = nodes[index.ind + 1].get<Phi_start>();
+
+            const Phihalf_predict& halfPredMinus = nodes[index.ind - 1].get<Phihalf_predict>();
+            const Phihalf_predict& halfPredPlus = nodes[index.ind + 1].get<Phihalf_predict>();
+            
+            const Phihalf_correct& halfCorrMinus = nodes[index.ind - 1].get<Phihalf_correct>();
+            const Phihalf_correct& halfCorrPlus = nodes[index.ind + 1].get<Phihalf_correct>();
+
+            const Phi_end_predict& endPredMinus = nodes[index.ind - 1].get<Phi_end_predict>();
+            const Phi_end_predict& endPredPlus = nodes[index.ind + 1].get<Phi_end_predict>();
+
+            const Position& posMinus = nodes[index.ind - 1].get<Position>();
+            const Position& posPlus = nodes[index.ind + 1].get<Position>();
+
+            double functionStart = fluid_function(startMinus.i, start.i, startPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+
+            double functionHalfPred = fluid_function(halfPredMinus.i, halfPred.i, halfPredPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+            
+            double functionHalfCorr = fluid_function(halfCorrMinus.i, halfCorr.i, halfCorrPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+            
+            double functionEndPred = fluid_function(endPredMinus.i, endPred.i, endPredPlus.i, 
+                posMinus.x, pos.x, posPlus.x);
+
+            endCorr.i = start.i + (TIME*(functionStart + 2*functionHalfPred + 2*functionHalfCorr 
+                + functionEndPred))/(6*STEPS);
+        });
 
     // This updates phi_start by replacing it with phi_end_correct
-    world.system()
-        .kind(Update);
-    
+    world.system<Phi_start, Phi_end_correct>()
+        .with<Middle_Tag>()
+        .kind(Update)
+        .each([](Phi_start initial, Phi_end_correct updated){
+            initial.i = updated.i;
+        });
+    */
+   
     // Set .txt file headers
     MyFile << "t, ";
     for (int x_step = 0; x_step < N; ++x_step) {
@@ -212,15 +262,15 @@ int main(int argc, char *argv[]) {
     
     // Run through systems every time step
     for (int t_step = 0; t_step <= STEPS; ++t_step) {
+        // Saves Data to a .txt file
         MyFile << t_step << ", ";
         for (int index = 0; index < N-1; ++index) {
-            const Position& p = nodes[index].get<Position>();
-            MyFile << p.x << ", ";
-            std::cout << p.x << ", ";
+            const Phi_start& phi = nodes[index].get<Phi_start>();
+            MyFile << phi.i << ", ";
         }
-        const Position& p = nodes[N-1].get<Position>();
-        MyFile << p.x << "\n";
-        std::cout << p.x << ", ";
+        const Phi_start& phi = nodes[N-1].get<Phi_start>();
+        MyFile << phi.i << "\n";
+
         world.progress();
     }
     
