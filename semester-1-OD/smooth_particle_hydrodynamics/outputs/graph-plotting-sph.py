OD 2/12/25 21:35. Updates: 1) Found and fixed error in force function, didn't have the correct gradient of W(r). 2) First attempt and particle in a box with a hole in it, boundary conditions aren't working as I would like and are behaving very weirdly. 3) System to check if the chosen (fixed) time step is too small.

semester-1-OD/smooth_particle_hydrodynamics/outputs/graph-plotting-sph.py
@@ -7,8 +7,6 @@ import os
 # Open data file
 file = open("/Users/oluwoledelano/ECS_Development/flecs-in-docker/Sketches/OD/smooth_particle_hydrodynamics/outputs/SPH_Dust.txt")
 
-# Define number of data points variable, arrays to store components
-
 # Read data in from file
 for i,row in enumerate(file): 
     if (i == 0):
@@ -36,18 +34,25 @@ for i,row in enumerate(file):
 
 # Graph formatting
 fig, ax_ani = plt.subplots()
-fig2, ax_hist = plt.subplots()
-plt.suptitle("Coupled Oscillator")
+#fig2, ax_hist = plt.subplots()
 
+plt.suptitle("Smooth Particle Hydrodynamics Simulation")
 ax_ani.set_title("Particles")
-ax_ani.set_xlim(np.min(x_matrix)-1,np.max(x_matrix))
-ax_ani.set_ylim(np.min(y_matrix),np.max(y_matrix))
+
+ax_ani.set_xlim(np.min(x_matrix)-1,np.max(x_matrix)+1)
+ax_ani.set_ylim(np.min(y_matrix)-1,np.max(y_matrix)+1)
+ax_ani.vlines(x = 0, ymin=0, ymax=5, color = 'black', label = 'axvline - full height')
+ax_ani.vlines(x = 5, ymin=0, ymax=2, color = 'black', label = 'axvline - full height')
+ax_ani.vlines(x = 5, ymin=3, ymax=5, color = 'black', label = 'axvline - full height')
+ax_ani.vlines(x = 10, ymin=0, ymax=5, color = 'black', label = 'axvline - full height')
+ax_ani.hlines(y = 0, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
+ax_ani.hlines(y = 5, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
 
 fig.tight_layout()
 
 # Animation
 graph_particles, = ax_ani.plot([],[],'o',color="Red",label = 'Ideal')
-_, _, graph_hist, = ax_hist.hist([],bins=50,density=True)
+#_, _, graph_hist, = ax_hist.hist([],bins=50,density=True)
 
 def particle(frame):
 
@@ -69,7 +74,7 @@ def histogram(frame):
 
     return graph_hist
 
-ani = FuncAnimation(fig, particle, frames=no_steps, interval=1)
-#ani2 = FuncAnimation(fig2, histogram, frames=no_steps, interval=1)
+ani = FuncAnimation(fig, particle, frames=no_steps, interval=0.001)
+#ani2 = FuncAnimation(fig2, histogram, frames=no_steps, interval=0.001)
 
 plt.show()