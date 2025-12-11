498cd5ca0f62a16de03d7c1afc0127b2f866b23d
Mattias Evans <mattiasevans@gmail.com>
2025-12-11 22:13:53 +0000

    error analysis for steady temperature complete

--- steady_state-Temperature/src/main.cpp
+++ steady_state-Temperature/src/main.cpp
@@ -7,10 +7,11 @@ solved analytically.
 #include <vector>
 #include <fstream> 
 #include <cmath>
+#include <iomanip>
 
 const double L = 0.01;          // Length
-const double N = 11;           // Number of nodes
-const int Pe = 1;               // Peclet Number
+const std::vector<int> nodeVector = {6, 11, 21, 41}; // Number of nodes
+const int Pe = 20;               // Peclet Number
 
 // Boundary Conditions
 double START = 273.15;
@@ -35,12 +36,13 @@ void matrix_filling(
     std::vector<double>& west,
     std::vector<double>& diagonal,
     std::vector<double>& east,
-    std::vector<double>& Q
+    std::vector<double>& Q,
+    int n
 ){
     phi.push_back(START);
-    for(int i = 1; i <= N-2; ++i) {
-    double A_W = west_function((i-1)*L/(N-1), i*L/(N-1), (i+1)*L/(N-1));
-    double A_E = east_function((i-1)*L/(N-1), i*L/(N-1), (i+1)*L/(N-1));
+    for(int i = 1; i <= n-2; ++i) {
+    double A_W = west_function((i-1)*L/(n-1), i*L/(n-1), (i+1)*L/(n-1));
+    double A_E = east_function((i-1)*L/(n-1), i*L/(n-1), (i+1)*L/(n-1));
     // Updates Matrix Diagonal
     diagonal.push_back(-A_W-A_E);
     // Initialize value of conserved Quantity
@@ -49,7 +51,7 @@ void matrix_filling(
     if (i == 1) {   
         east.push_back(A_E);
         Q.push_back(-A_W * START);
-    } else if (i == N-2) {
+    } else if (i == n-2) {
         west.push_back(A_W);
         Q.push_back(-A_E * END);
     } else {
@@ -67,13 +69,14 @@ void matrix_solving(
     std::vector<double>& west,
     std::vector<double>& diagonal,
     std::vector<double>& east,
-    std::vector<double>& Q
+    std::vector<double>& Q,
+    int n
 ){
-    for(int i = 0; i <= N-2; ++i) {
+    for(int i = 0; i <= n-2; ++i) {
         if (i == 0) {
             east[i] = east[i] / diagonal[i];
             Q[i] = Q[i] / diagonal[i];
-        } else if (i == N - 2) {
+        } else if (i == n - 2) {
             Q[i] = (Q[i] - 
                 west[i-1]*Q[i-1]) / (diagonal[i] - west[i-1]*east[i-1]);
         } else {
@@ -82,8 +85,8 @@ void matrix_solving(
                 west[i-1]*Q[i-1]) / (diagonal[i] - west[i-1]*east[i-1]);
         }
     }
-    for(int i = N - 2; i >= 1; --i) {
-        if (i == N - 2 ) {
+    for(int i = n - 2; i >= 1; --i) {
+        if (i == n - 2 ) {
             phi[i] = Q[i-1];
         } else {
             phi[i] = Q[i-1] - east[i-1] * phi[i+1];
@@ -92,43 +95,48 @@ void matrix_solving(
 }
 
 int main() {
-    // Prepare matrices and vectors
-    // sparse matrix
-    std::vector<double> west_steady;
-    std::vector<double> diagonal_steady;
-    std::vector<double> east_steady;
+    for (int N : nodeVector) {
+        // Prepare matrices and vectors
+        // sparse matrix
+        std::vector<double> west_steady;
+        std::vector<double> diagonal_steady;
+        std::vector<double> east_steady;
 
-    // vectors
-    std::vector<double> Q_steady;
-    std::vector<double> phi_steady;
+        // vectors
+        std::vector<double> Q_steady;
+        std::vector<double> phi_steady;
 
-    // Prepare Save File
-    std::ofstream MyFile;
-    MyFile.open("temperature.txt");
-    if (!MyFile.is_open())
-    {
-        std::cout<<"Error in creating file"<<std::endl; 
-        return 1; 
+        // Prepare Save File
+        std::ofstream MyFile;
+        std::string fileName="temperature,N=" + std::to_string(N) + ".txt";
+        MyFile.open(fileName);
+        if (!MyFile.is_open())
+        {
+            std::cout<<"Error in creating file"<<std::endl; 
+            return 1; 
+        }
+
+        MyFile << "Node position, Conserved Quantity" << std::endl;
+
+        matrix_filling(
+            phi_steady,
+            west_steady, 
+            diagonal_steady, 
+            east_steady, 
+            Q_steady, N);
+
+        matrix_solving(
+            phi_steady,
+            west_steady, 
+            diagonal_steady, 
+            east_steady, 
+            Q_steady, N);
+        
+        for (int i = 0; i<=N-1; ++i) {
+            MyFile << i*L/(N-1) << ", " << std::setprecision(17) << phi_steady[i] << std::endl; 
+        }
+        MyFile.close();
+        	 
     }
-
-    MyFile << "Node position, Conserved Quantity" << std::endl;
-
-    matrix_filling(
-        phi_steady,
-        west_steady, 
-        diagonal_steady, 
-        east_steady, 
-        Q_steady);
-
-    matrix_solving(
-        phi_steady,
-        west_steady, 
-        diagonal_steady, 
-        east_steady, 
-        Q_steady);
     
-    for (int i = 0; i<=N-1; ++i) {
-        MyFile << i*L/(N-1) << ", " << phi_steady[i] << std::endl; 
-    }
-    MyFile.close();
 }
--- steady_state-Temperature/src/plotting.py
+++ steady_state-Temperature/src/plotting.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import os
 
 L = 0.01 # Length of system
-Nx = 11 # Number of nodes
-Pe = 1   # Peclet Number
+N = np.array([41, 21, 11]) # Number of nodes
+Pe = 20  # Peclet Number
 
 #Boundary Conditions
 Start = 273.15 # Temperature at start
@@ -17,39 +18,90 @@ def analytical_model(x, Pe_num, start, end):
 
 # Open data file and generate array
 root_folder = os.path.dirname(os.path.dirname(__file__))
-data_path = os.path.join(root_folder, "outputs", "temperature.txt")
-save_pathpdf = os.path.join(root_folder, "outputs", f"Pe={Pe},Nodes={Nx}.pdf")
-save_pathpng = os.path.join(root_folder, "outputs", f"Pe={Pe},Nodes={Nx}.png")
-data = np.genfromtxt(data_path, delimiter = ",", skip_header = 1)
+data_path1 = os.path.join(root_folder, "outputs", "temperature,N=41.txt")
+data_path2 = os.path.join(root_folder, "outputs", "temperature,N=21.txt")
+data_path3 = os.path.join(root_folder, "outputs", "temperature,N=11.txt")
+save_path = os.path.join(root_folder, "outputs", f"Pe={Pe}.pdf")
+data1 = np.genfromtxt(data_path1, delimiter = ",", skip_header = 1)
+data2 = np.genfromtxt(data_path2, delimiter = ",", skip_header = 1)
+data3 = np.genfromtxt(data_path3, delimiter = ",", skip_header = 1)
 
 # Creates data according to the analytical model
 x_model = np.linspace(0, L, num = 1000)
 y_model = analytical_model(x_model, Pe, Start, End)
 
 # Creates data according to the computational model
-x_computational = data[:,0]
-y_computational = data[:,1]
+x_computational1 = data1[:,0]
+T_computational1 = data1[:,1]
+x_computational2 = data2[:,0]
+T_computational2 = data2[:,1]
+x_computational3 = data3[:,0]
+T_computational3 = data3[:,1]
 
-# Compares the analytical and computation model in a plot
-plt.figure()
-plt.plot(x_model, y_model, color = "k", linestyle = "-", label = "Exact")
-plt.plot(x_computational, y_computational, color = "k", linestyle = "--", label = "Calculated")
-ax = plt.gca()
-# Set position and bounds of axes
-ax.spines['bottom'].set_position(('data', Start))
-ax.spines['top'].set_position(('data', End))
-ax.spines['left'].set_bounds(Start, End)
-ax.spines['right'].set_bounds(Start, End)
-# Set ticks in correct position
-ax.xaxis.set_ticks_position('bottom')
-ax.xaxis.set_label_position('bottom')
-ax.yaxis.set_ticks([tick for tick in ax.get_yticks() if (tick >= Start and tick <= End)])
-plt.xlim(min(x_model), max(x_model))
-plt.xlabel('x (m)')
-plt.ylabel('T (K)')
-#ax.yaxis.set_label_coords(-0.1, 0.6)
-plt.legend(loc='upper left', bbox_to_anchor=(0, 0.95))
-plt.savefig(save_pathpdf, dpi = 1200)
-plt.savefig(save_pathpng, dpi = 1200)
+comparison1_T1 = T_computational1[0::4][1:-1]
+comparison2_T1 = T_computational1[0::2][1:-1]
+comparison_T2 = T_computational2[0::2][1:-1]
+comparison_T3 = T_computational3[0::2][1:-1]
+
+# After running once with Pe = 1 it was confirmed p = 2
+"""p_list = np.log((comparison_T2-T_computational3[1:-1])/(comparison1_T1-
+                                                         comparison_T2))/np.log(2)
+p = np.mean(p_list)"""
+p = 2
+
+# error is (T_h - T_2h) / (2^p - 1)
+errors1 = np.abs((comparison2_T1-T_computational2[1:-1]))/(2**p-1)
+chiSquared1 = 0
+for i in range(len(T_computational2[1:-1])):
+    T_observed = comparison2_T1[i]
+    x = x_computational2[i+1]
+    error = errors1[i]
+    T_analytical = analytical_model(x, Pe, Start, End)
+    chi_value = (T_observed-T_analytical)**2/error**2
+    chiSquared1 += chi_value
+reducedChi1 = chiSquared1/len(T_computational2[1:-1])
+# The fixed points have no error
+errors1 = np.concatenate([[0],errors1,[0]])
+
+errors2 = (np.abs(comparison_T2-T_computational3[1:-1]))/(2**p-1)
+chiSquared2 = 0
+for i in range(len(T_computational3[1:-1])):
+    T_observed = comparison_T2[i]
+    x = x_computational3[i+1]
+    error = errors2[i]
+    T_analytical = analytical_model(x, Pe, Start, End)
+    chi_value = (T_observed-T_analytical)**2/error**2
+    chiSquared2 += chi_value
+reducedChi2 = chiSquared2/len(T_computational3[1:-1])
+errors2 = np.concatenate([[0],errors2,[0]])
+
+print(reducedChi1)
+print(reducedChi2)
+# Formatting preamble
+cm = 1/2.54 #cm in inches
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams.update({'font.size': 14})
+# Plotting
+fig, axs = plt.subplots(1, 2, figsize=(17*cm, 10*cm), layout="constrained")
+
+axs[0].plot(x_model, y_model, color = "tab:red", linestyle = "-", label = "Exact")
+axs[0].errorbar(x_computational2, T_computational1[0::2], yerr=errors1, 
+             color = "k", fmt = "x", label = "Calculated")
+axs[0].set_title('41 nodes')
+axs[0].tick_params(axis='both', direction='in')
+
+axs[1].plot(x_model, y_model, color = "tab:red", linestyle = "-", label = "Exact")
+axs[1].errorbar(x_computational3, T_computational2[0::2], yerr=errors2, 
+             color = "k", fmt = "x", label = "Calculated")
+axs[1].tick_params(axis='both', direction='in')
+axs[1].set_title('21 nodes')
+
+handles, labels = axs[1].get_legend_handles_labels()
+leg = fig.legend(handles, labels, loc='upper center',
+                 bbox_to_anchor=(0.5, 0), ncol = 3)
+for ax in axs.flat:
+    ax.set(xlabel=r'x/m', ylabel='T/K')
+for ax in axs.flat:
+    ax.label_outer()
+fig.savefig(save_path, bbox_inches = 'tight')
 plt.show()
-plt.close()
