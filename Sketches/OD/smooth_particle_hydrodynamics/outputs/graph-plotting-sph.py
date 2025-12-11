11/12/25 22:49 OD. UPDATES: 1) Strange attractor (which was actually more of a strange repulsor, there was a sign error) problems seems to be fixed. It seems that its a result of choices made rgarding the kernel function. Its worse for splines than for gaussian kernels, the repulsive force on splines is too strong. 2) Error analysis by testing how well density smoothing approximation works by testing it against a known density distribution. 3) Found some sign errors and missing factors while searching for strange attractor.

Sketches/OD/smooth_particle_hydrodynamics/outputs/graph-plotting-sph.py
@@ -5,9 +5,19 @@ from matplotlib.animation import FuncAnimation
 import os
 import scipy.constants as const
 
+"""
+Graph plotting for smoothed particle hydrodynamics code
+
+Some potential additions:
+Contouring irregularly spaced data: 
+    - https://matplotlib.org/stable/gallery/images_contours_and_fields/irregulardatagrid.html
+"""
+
 # Open data file
 file = open("/Users/oluwoledelano/ECS_Development/flecs-in-docker/Sketches/OD/smooth_particle_hydrodynamics/outputs/SPH_Dust.txt")
 specs_file = open("/Users/oluwoledelano/ECS_Development/flecs-in-docker/Sketches/OD/smooth_particle_hydrodynamics/outputs/sph_code_specifications.txt"); 
+density_file = open("/Users/oluwoledelano/ECS_Development/flecs-in-docker/Sketches/OD/smooth_particle_hydrodynamics/outputs/density_field.txt")
+density_gambel = open("/Users/oluwoledelano/ECS_Development/flecs-in-docker/Sketches/OD/smooth_particle_hydrodynamics/outputs/gambel_density_field.txt")
 
 # Read data in from file
 for i,row in enumerate(specs_file): 
@@ -25,13 +35,13 @@ for i,row in enumerate(specs_file):
         GX2 = int(row[1])
         GY = int(row[2])
 
+# Define matrices to store positions
 x_matrix = np.zeros((no_steps,no_particles))
 y_matrix = np.zeros((no_steps,no_particles))
 x_vel_matrix = np.zeros((no_steps,no_particles))
 y_vel_matrix = np.zeros((no_steps,no_particles))
-density_matrix = np.zeros((GY,(GX+GX2)))
-
-no_hist_bins = int(no_particles)
+density_matrix = np.zeros((GY+1,(GX+GX2)+1))
+gambel_matrix = np.zeros((15,2))
 
 for i,row in enumerate(file): 
     if(i == 0):
@@ -47,32 +57,49 @@ for i,row in enumerate(file):
                 x_vel_matrix[i-1][j] = float(pos_vector[2])
                 y_vel_matrix[i-1][j] = float(pos_vector[3])
 
-for i,row in enumerate(file): 
+for i,row in enumerate(density_file): 
     row = row.split(",")
-        
-    for j in row:
-        density_matrix[i][j] = float(row[j])
+    for j,rho in enumerate(row):
+        if(i < 5):
+            density_matrix[i][j] = float(rho)
 
-plt.contourf(density_matrix)
-plt.colorbar()
+for i,row in enumerate(density_gambel): 
+    row = row.split(",")
+    for j,rho in enumerate(row):
+            gambel_matrix[i][j] = float(rho)
 
-# Maxwell-Boltzmann Distribution
+print(gambel_matrix)
+
+# Plot the Maxwell-Boltzmann Distribution
 k = 1.380649 * 10**-23
-peak = ((2 * k * Temp) / Mass ) ** 0.5
+v_rms = (( k * Temp) / Mass ) ** 0.5
+velocities = np.linspace(-1000, 1000, 2000)
 
-x = np.linspace(0, 25, 100) 
-velocities = np.linspace(0, 4000, 1000)
+def maxwell_boltzman_velocity(v):
+    N = (Mass / (2 * np.pi * k * Temp))**1.5 
+    return N * np.exp( - (Mass * v**2 ) / (2 * k * Temp) )
 
-def maxwell_boltzman(v):
-    N = 4 * np.pi * (v**2) * (Mass / (2 * np.pi * k * Temp))**1.5 
-    return N * np.exp( - (Mass * v**2 ) / (2 * k * Temp))
+def maxwell_boltzman_speed(v):
+    N = 4 * np.pi * (Mass / (2 * np.pi * k * Temp))**1.5 
+    return N * v**2 * np.exp( - (Mass * v**2 ) / (2 * k * Temp) )
 
 # Graph formatting
 fig, ax_ani = plt.subplots()
 fig2, ax_hist = plt.subplots()
-
+fig3, ax_density = plt.subplots()
+fig4, ax_gambel = plt.subplots()
 ax_ani.set_title("Particles")
 
+ax_gambel.scatter(gambel_matrix[:,0], gambel_matrix[:,1])
+
+# Contour plot for density 
+density = ax_density.contourf(density_matrix)
+fig3.colorbar(density,ax=ax_density)
+
+# Edit number of bins for histogram
+no_hist_bins = int(no_particles / 3) 
+
+# Draw box
 ax_ani.set_xlim(np.min(x_matrix)-1,np.max(x_matrix)+1)
 ax_ani.set_ylim(np.min(y_matrix)-1,np.max(y_matrix)+1)
 ax_ani.vlines(x = 0, ymin=0, ymax=5, color = 'black', label = 'axvline - full height')
@@ -82,13 +109,21 @@ ax_ani.vlines(x = 10, ymin=0, ymax=5, color = 'black', label = 'axvline - full h
 ax_ani.hlines(y = 0, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
 ax_ani.hlines(y = 5, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
 
+ax_density.vlines(x = 0, ymin=0, ymax=5, color = 'black', label = 'axvline - full height')
+ax_density.vlines(x = 5, ymin=0, ymax=2, color = 'black', label = 'axvline - full height')
+ax_density.vlines(x = 5, ymin=3, ymax=5, color = 'black', label = 'axvline - full height')
+ax_density.vlines(x = 10, ymin=0, ymax=5, color = 'black', label = 'axvline - full height')
+ax_density.hlines(y = 0, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
+ax_density.hlines(y = 5, xmin=0, xmax=10, color = 'black', label = 'axvline - full height')
+
 fig.tight_layout()
 
 # Animation
-graph_particles, = ax_ani.plot([],[],'o',color="Red",label = 'Ideal')
-# graph_maxwell, = ax_hist.plot(velocities, maxwell_boltzman(velocities)) # Scale of this plot is much smaller than hist
-_, _, graph_hist, = ax_hist.hist([],bins=no_hist_bins,density=True,color='b')
-# ax_hist.vlines(x = peak, ymin=0, ymax=max(maxwell_boltzman(velocities))+0.0001, color = 'black', label = 'axvline - full height')
+graph_particles, = ax_ani.plot([],[],'o',color="Red",label = 'Ideal gas')
+graph_maxwell, = ax_hist.plot(velocities, 2*10**6 * maxwell_boltzman_velocity(velocities),color = 'blue') # Scale of this plot is much smaller than hist
+_, _, graph_hist_x, = ax_hist.hist([],bins=no_hist_bins,density=True,color='b')
+ax_hist.scatter(v_rms, 2*10**6 * maxwell_boltzman_velocity(v_rms),color='red',marker='x')
+# ax_hist.vlines(x = v_rms, ymin=0, ymax= 1*10**6 * max(maxwell_boltzman_velocity(velocities)) + 0.003, color = 'black', label = 'axvline - full height')
 
 def particle(frame):
     state_x = x_matrix[frame,:]
@@ -103,9 +138,8 @@ def histogram(frame):
     state_vy = y_vel_matrix[frame,:]
     speed = np.sqrt( state_vx**2 + state_vy**2 )
 
-    _, _, graph_hist, = ax_hist.hist(speed,bins=no_hist_bins,density=True,color='b')
-
-    # return graph_hist
+    _, _, graph_hist_x, = ax_hist.hist(state_vx,bins=no_hist_bins,density=True,color='b')
+    graph_maxwell, = ax_hist.plot(velocities, maxwell_boltzman_velocity(velocities))
 
 ani = FuncAnimation(fig, particle, frames=no_steps, interval=0.001)
 ani2 = FuncAnimation(fig2, histogram, frames=no_steps, interval=0.001)