9886592a605d266c09bc4190350607d0a456e4d8
Michael Sparks <sparks.m@gmail.com>
2025-12-19 04:21:20 +0100

    Remove the duplicated code infrastructur

--- examples/semester-1-OD/SPH_Euler/docs/gravity_presentation/build-notes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-( cat presentation.md ; echo '\newpage'; echo '# Full Source'; echo '```'; cat ../../src/asteroids_knn.cpp ; echo '```' ) | pandoc --pdf-engine=xelatex default-notes.yaml -  -o presentation-notes.pdf
